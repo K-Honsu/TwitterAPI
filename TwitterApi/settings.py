f41bd4fbc4625caaf4c78a3229dad423765731f4
P-Horus <feranmia51@gmail.com>
2023-04-06 11:24:04 +0100

    fix env file and add swagger ui

--- TwitterApi/settings.py
+++ TwitterApi/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+lzm5+w6xkpt#lcra7(71gps@+@^80ki2&x0ik7*tv_azar16*'
+SECRET_KEY = os.environ.get('SECRET_KEY_TWITTER')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- TwitterApi/urls.py
+++ TwitterApi/urls.py
@@ -2,13 +2,29 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Twitter Api",
+        default_version="1.0.0",
+        description="API DOCUMENTATION OF TWITTER API",
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('tweet/', include('tweet.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include([
+        path('users/', include('users.urls')),
+        path('tweet/', include('tweet.urls')),
+        path('auth/', include('djoser.urls')),
+        path('auth/', include('djoser.urls.jwt')),
+        path('docs/', schema_view.with_ui('swagger',
+             cache_timeout=0), name='swagger-schema')
+    ]))
+
 ]
 
 
