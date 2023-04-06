da1d6dea3d910a08e974d426ae4ccc61c5ed943f
P-Horus <feranmia51@gmail.com>
2023-04-06 11:52:59 +0100

    add static files

--- TwitterApi/settings.py
+++ TwitterApi/settings.py
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
